0deb71af71fee2a8efb6300a9474463bfbac3298
Suyog Ghate <suyogghate96@gmail.com>
2024-07-16 16:09:21 +0000

    datatypes basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all JS code as newer version 
+
+// alert(3+3);   // we are using node js not browser
+
+console.log(5+8);
+console.log("Suyog");
+
+let name = "Amar";
+let age = 78;
+let isLoggedIn = true;
+let state;
+
+
+
+// number => 2 to power 53
+// bigint 
+// string => "sdfvds"
+// boolean => true/false
+// null => standalone value
+// undefined => (state)
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
