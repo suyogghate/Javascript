1ac8597df9513d5c20590d40bdec78f4fbf0eb24
Suyog Ghate <suyogghate96@gmail.com>
2024-07-17 12:06:02 +0000

    type conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "suyog";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "suyog";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "suyog" => true
+
+let someNumber = 12;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
