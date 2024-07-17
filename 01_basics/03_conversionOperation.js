72b3970f98f431c69ae295af3cf5b954943c9aa1
Suyog Ghate <suyogghate96@gmail.com>
2024-07-17 12:30:36 +0000

    basic operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,9 +13,9 @@ let valueInNumber = Number(score);
 
 // let isLoggedIn = 1;
 // let isLoggedIn = "";
-let isLoggedIn = "suyog";
+// let isLoggedIn = "suyog";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 // console.log(booleanIsLoggedIn);
 
@@ -23,9 +23,56 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 // "" => false
 // "suyog" => true
 
-let someNumber = 12;
+// let someNumber = 12;
 
-let stringNumber = String(someNumber);
+// let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//------------------- Operations -------------------------
+
+let value = 5;
+
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Suyog";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");   //see documentation
+
+// console.log((3 + 4) * 5 % 3);  //avoid this kind of code anywhere
+
+// console.log(+true);
+// console.log(+false);
+
+// console.log(+"");     //avoid this
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+//refer mdn web docs (prefix, postfix)
+
