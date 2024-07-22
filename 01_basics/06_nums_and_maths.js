9f9ae7bb061cbf334581bf2ed68e60884db5e71e
Suyog Ghate <suyogghate96@gmail.com>
2024-07-22 09:29:08 +0000

    number and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+// const score = 500
+// console.log(score);
+
+// const balance = new Number(400);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 125.7894;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundred = 1000000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4, 6, 8, 1));
+// console.log(Math.max(4, 6, 8, 1));
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
