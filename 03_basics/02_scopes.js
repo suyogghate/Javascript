88d9aeccd339a26c1d3b9a78a99eb32fe4478f50
Suyog Ghate <suyogghate96@gmail.com>
2024-07-28 11:10:42 +0000

    closures intro

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,47 @@ let a = 300;
 if(true){
     let a = 10;
     const b = 20;
-    console.log(a);
+    // console.log(a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
+
+function one(){
+    const username = "suyog"
+
+    function two()
+    {
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+if(true){
+    const username = "suyog"
+    if(username === "suyog"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// =============================================
+console.log(addone(6));
+
+function addone(num){
+    return num + 1
+}
+
+addtwo(5);
+
+const addtwo = function(num){
+    return num + 2
+}
