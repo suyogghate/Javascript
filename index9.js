7d7b027be5934911b0c37b4ec93ca04864fd1d8f
suyogghate <suyogghate96@gmail.com>
2025-03-11 17:47:08 +0530

    desctructuring arrays/objects, desctructure in function parameters.

--- index9.js
+++ index9.js
@@ -109,3 +109,63 @@
 // console.log(rectangle.heigth);
 // console.log(rectangle.area);
 
+// destructuring = extract values from arrays and objects,
+//                  then assign them to variables in a convenient way
+//                  [] = to perform array destructuring
+//                  {} = to perform object destructuring
+
+// Swap the value of two variables
+// let a = 1;
+// let b = 2;
+
+// [a, b] = [b, a];
+
+// console.log(a);
+// console.log(b);
+
+// Swap two elements in an array
+// const colors = ["red", "green", "blue", "black", "white"];
+
+// [colors[0], colors[4]] = [colors[4], colors[0]];
+
+// console.log(colors);
+
+
+// Assign array elements to variables
+// const colors = ["red", "green", "blue", "black", "white"];
+
+// const [firstColor, secondColor, thirdColor, ...extraColors] = colors;
+
+// console.log(firstColor, secondColor, thirdColor);
+// console.log(extraColors);
+
+// Extract values from objects
+// const person1 = {
+//     firstName: "Spongebob",
+//     lastName: "Sqaurepants",
+//     age: 30,
+//     job: "Fry Cook"
+// }
+
+// const person2 = {
+//     firstName: "Patrick",
+//     lastName: "Star",
+//     age: 24,
+// }
+
+// const {firstName, lastName, age, job="Unemployed"} = person2;
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+// console.log(job);
+
+// Destructure in function parameters
+
+// function displayPerson({firstName, lastName, age, job="Unemployed"}){
+//     console.log(`Name: ${firstName} ${lastName}`);
+//     console.log(`age: ${age}`);
+//     console.log(`job: ${job}`);
+// }
+
+// displayPerson(person1);
